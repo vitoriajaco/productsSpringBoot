d87c380dd625e9f66955eac3523af1047069de8d
vitoriajaco <vitoriajaco@gmail.com>
2023-03-15 16:20:59 -0300

    Exemplo de mapping

--- src/main/java/cursoSpring/Products/rest/controller/ClienteController.java
+++ src/main/java/cursoSpring/Products/rest/controller/ClienteController.java
@@ -1,18 +1,20 @@
 package cursoSpring.Products.rest.controller;
 
+import cursoSpring.Products.entity.Cliente;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/api/clientes")
+
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/clientes/hello/{nome}",
+            method = RequestMethod.POST,
+            consumes = {"application/json", "application/xml"}, // O cliente poderá uma requisição tanto json quanto xml
+             produces= {"application/json", "application/xml"}
+    )
     @ResponseBody
-    public String helloCliente (@PathVariable ("nome") String nomeCliente){
+    public String helloCliente (@PathVariable ("nome") String nomeCliente, @RequestBody Cliente cliente){
         return String.format("Hello %s ", nomeCliente);
     }
 
