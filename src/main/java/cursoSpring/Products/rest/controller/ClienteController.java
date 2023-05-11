ce262b852114f610c2ca4fbac0daa2eff622a601
vitoriajaco <vitoriajaco@gmail.com>
2023-05-11 20:58:41 -0300

    Refatorando controller tornando mais limpo com metodo Rest

--- src/main/java/cursoSpring/Products/rest/controller/ClienteController.java
+++ src/main/java/cursoSpring/Products/rest/controller/ClienteController.java
@@ -4,15 +4,16 @@ import cursoSpring.Products.entity.Cliente;
 import cursoSpring.Products.repository.Clientes;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
-@RequestMapping
+@RestController //Usando o RestController nao se mostra mais necessario usar a anotação ResponseBody
+@RequestMapping (path = "/api/clientes") // Passando o parametro aqui evita repetir em cima de cada metodo
 public class ClienteController {
 
 
@@ -24,65 +25,54 @@ public class ClienteController {
 
     }
 
-    @GetMapping
-    @ResponseBody
-    public String metodo(){
-        return "qualquer coisa";
-    }
+    @GetMapping(path = "/{id}")
+    public Cliente getClienteById(@PathVariable Integer id) {// Esse id vai estar na URL
 
-
-    @GetMapping(path = "/api/clientes/{id}")
-    @ResponseBody
-    public ResponseEntity getClienteById(@PathVariable Integer id) {// Esse id vai estar na URL
-        Optional<Cliente> cliente = clientes.findById(id);
-        if (cliente.isPresent()){
-            return ResponseEntity.ok(cliente.get());
+            return clientes.findById(id)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
         }
-        return ResponseEntity.notFound().build();
-    }
 
-    @PostMapping(path = "/api/clientes")
-    @ResponseBody //Resposta da Requisição não confundir com o Request que vem na requisição que é o que recebe
-    public ResponseEntity save (@RequestBody Cliente cliente) { //Esse (Cliente) vem no corpo da requisição
-        Cliente clienteSalvo = clientes.save(cliente);
-        return ResponseEntity.ok(clienteSalvo); //Ainda nao esta no modelo restfull
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente save (@RequestBody Cliente cliente) { //Esse (Cliente) vem no corpo da requisição
+       return clientes.save(cliente);
 
     }
 
-    @DeleteMapping(path = "/api/clientes/{id}")
-    @ResponseBody
-    public ResponseEntity delete (@PathVariable Integer id) { //O delete só recebe um id no corpo da requisição
-        Optional<Cliente> cliente = clientes.findById(id);
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete (@PathVariable Integer id) {
+        clientes.findById(id).map(cliente -> { //metodo map necessita retornar algo
+            clientes.delete(cliente);
+            return cliente;
+                })
+                .orElseThrow(()
+                        -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente Nao encontrado"));
 
-        if (cliente.isPresent()) {
-            clientes.delete(cliente.get());
-            return ResponseEntity.noContent().build(); //Se o código funcionar retorna "204 no content"
-        }
-        return ResponseEntity.notFound().build();
     }
 
-
-    @PutMapping(path = "/api/clientes/{id}")
-    @ResponseBody
-    public ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente) {
-        return clientes.findById(id)
+    @PutMapping(path = "/{id}")
+    public void update(@PathVariable Integer id, @RequestBody Cliente cliente) {
+         clientes.findById(id)
                 .map(clienteExistente -> { //O Optional tem o método map, se existir o resultado, ele entra no map e executa
                     //o que está no map. Caso contrario ele retorna outra coisa.
                     cliente.setId(clienteExistente.getId()); //Troca o id para nao precisar passar todos os dados get
                     clientes.save(cliente);
-                    return ResponseEntity.noContent().build();
-                }).orElseGet(() -> ResponseEntity.notFound().build()); //O suplier nao recebe nada e retorna o que vc passar.
+                    return clienteExistente;
+                }) .orElseThrow(()
+                         -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente Nao encontrado"));
 
     }
 
-    @GetMapping(path = "/api/clientes")
-    public ResponseEntity find (Cliente filtro) {
+    @GetMapping
+    public List<Cliente> find (Cliente filtro) {
         ExampleMatcher matcher = ExampleMatcher //permite que ele encontre os clientes atraves das propriedades
                 .matching() //
                 .withIgnoreCase() //Os valores que forem strings independentemente de estar maiusculo ou minusculo serão considerados na hora de encontrar
                 .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING); //Forma que vai encontrar os valores String - em qualquer posição que esteja o valor ele ira ser retornado
         Example example = Example.of(filtro, matcher);
-        List<Cliente> lista = clientes.findAll(example);
-        return ResponseEntity.ok(lista);
+       return  clientes.findAll(example);
+
     }
 }
